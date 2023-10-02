1b8d22a22f1436a69f3a4cf339d1ddbd70566c5f
wwwas <wwwas@DESKTOP-0IPDBEB>
2023-10-02 10:39:07 +0600

    Добавьте файлы проекта.

---
+++ Satbayev.DAL/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Satbayev.DAL
+{
+    internal class Account
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int ClientId { get; set; }
+        public string IBAN { get; set; }
+        public DateTime ExpariDate { get; set; }
+        public int Currency { get; set; }
+        private double _Balance;
+        public double Balance
+        {
+            get
+            {
+                return _Balance;
+            }
+            set
+            {
+                if(value < 0) _Balance = 0;
+                else _Balance = value;
+            }
+        }
+        public bool IsActivity { get; set; } = true;
+        public bool CanpPayOnline { get; set; } = false;
+    }
+}
---
+++ Satbayev.DAL/Client.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Satbayev.DAL
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string ShortName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(LastName))//Sattar B.
+                {
+                    return string.Format("{0} {1}.",
+                        FirstName, MiddleName[0]);
+                }
+                else//Sattar B.B.
+                {
+                    return string.Format("{0} {1}. {2}.",
+                        FirstName, MiddleName[0], LastName[0]);
+                }
+
+            }
+        }
+        public string FullName { get { return FullName; } }
+        public DateTime BirstDay { get; set; }
+        public int GetAge
+        {
+            get
+            {
+                return DateTime.Now.Year - BirstDay.Year;
+            }
+        }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string INN { get; set; }
+        public Adress Adress { get; set; }
+
+    }
+
+
+}
