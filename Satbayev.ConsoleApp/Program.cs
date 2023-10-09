a7b96370a57c00ca10315f6fa8fe24a0413b5528
Бекнур Саттар <www.asemush@gmail.com>
2023-10-09 10:51:03 +0600

    test3

--- Satbayev.ConsoleApp/Program.cs
+++ Satbayev.ConsoleApp/Program.cs
@@ -12,29 +12,70 @@ namespace Satbayev.ConsoleApp
     {
         public static string Path = "";
 
-        static void Main()
+        static void Main(string[] args)
         {
             Console.Write("Enter Pat for Batabase: ");
             Path = Console.ReadLine();
             ServiceClient service = new ServiceClient(Path);
-            Client client = new Client();
-            client.INN = "030318500265";
-            client.Email = "www.asemush@gmail.com";
-            client.FirstName = "Sattar";
-            client.MiddleName = "Beknur";
-            client.LastName = "Bekzhanuli";
-            client.BirstDay = new DateTime(2003, 03, 03);
-            client.CreateDate = DateTime.Now;
-            client.Adress = new Adress()
+            Console.WriteLine("(1 - Авторизация) ");
+            Console.WriteLine("(2 - Регистрация) ");
+            string Question = Console.ReadLine();
+            if(Question == "1")
             {
-                Country = "Kazakhstan",
-                City = "Almaty",
-                House = "Taugul 2",
-                Region = "Orbita",
-                Street = "Riskulbekova"
-            };
+                Console.WriteLine("Введите ваш логин");
+                string login = Console.ReadLine();
 
-            service.Registration(client);
+                Console.WriteLine("Введите ваш пароль:");
+                string password = Console.ReadLine();
+
+                Client cleint = service.Auth(login, password);
+
+                if(cleint == null)
+                {
+                    Console.WriteLine("Ваш логин или пароль неправелный!!!");
+                }
+                else
+                {
+                    Console.WriteLine("Привет" + cleint.FirstName);
+                }
+            }
+            else if(Question == "2")
+            {
+                Client client = new Client();
+
+                Console.Write("Введите ИИН:");
+                client.INN = Console.ReadLine();
+
+                Console.Write("Введите Почту:");
+                client.Email = Console.ReadLine();
+
+                Console.Write("Введите Имя:");
+                client.FirstName = Console.ReadLine();
+
+                Console.Write("Введите Фамилия:");
+                client.MiddleName = Console.ReadLine();
+
+                Console.Write("Введите Очество:");
+                client.LastName = Console.ReadLine();
+
+                Console.Write("Введите Дату рождения:");
+                client.BirstDay = DateTime.Parse(Console.ReadLine());
+                
+                client.CreateDate = DateTime.Now;
+
+                Console.Write("Введите Адрес(Страна, Город, Дом, Регион, Улица, код региона):");
+                client.Adress = new Adress()
+                {
+                    Country = Console.ReadLine(),
+                    City = Console.ReadLine(),
+                    House = Console.ReadLine(),
+                    Region = Console.ReadLine(),
+                    Street = Console.ReadLine(),
+                    PostalCode = Console.ReadLine()
+                };
+
+                service.Registration(client);
+            }
         }
     }
 }
