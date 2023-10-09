ac6e06fd9bee7b4011f62c41e678105f89a06d31
Бекнур Саттар <www.asemush@gmail.com>
2023-10-09 10:21:42 +0600

    test2

--- Satbayev.ConsoleApp/Program.cs
+++ Satbayev.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Satbayev.BLL;
+using Satbayev.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +10,31 @@ namespace Satbayev.ConsoleApp
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
+        public static string Path = "";
 
+        static void Main()
+        {
+            Console.Write("Enter Pat for Batabase: ");
+            Path = Console.ReadLine();
+            ServiceClient service = new ServiceClient(Path);
+            Client client = new Client();
+            client.INN = "030318500265";
+            client.Email = "www.asemush@gmail.com";
+            client.FirstName = "Sattar";
+            client.MiddleName = "Beknur";
+            client.LastName = "Bekzhanuli";
+            client.BirstDay = new DateTime(2003, 03, 03);
+            client.CreateDate = DateTime.Now;
+            client.Adress = new Adress()
+            {
+                Country = "Kazakhstan",
+                City = "Almaty",
+                House = "Taugul 2",
+                Region = "Orbita",
+                Street = "Riskulbekova"
+            };
+
+            service.Registration(client);
         }
     }
 }
